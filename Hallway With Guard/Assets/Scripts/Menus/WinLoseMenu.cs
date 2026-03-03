5aba2540ddfca3f987fc9873805fb830d267ab03
Chandler Guzman <ninthewander@gmail.com>
2026-03-03 18:05:25 -0500

    fixed UI bug

--- Hallway With Guard/Assets/Scripts/Menus/WinLoseMenu.cs
+++ Hallway With Guard/Assets/Scripts/Menus/WinLoseMenu.cs
@@ -3,6 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class WinLoseMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    
     public void PlayAgain()
     {
         SceneManager.LoadScene(1);
